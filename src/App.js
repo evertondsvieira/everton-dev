42805a8ada9b7e30a134e1813538da3b015dfed3
Everton Vieira <vieiraeverton33@hotmail.com>
2022-07-31 20:08:31 -0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './reset.css'
+
+function App() {
+  return (
+    <div className="App">
+      <img className="wallpaper" src="./wallpaper.png" alt="wallpaper"></img>
+      <div className="centralize"><h1 className="text">Coming soon</h1></div>
+    </div>
+  );
+}
+
+export default App;
