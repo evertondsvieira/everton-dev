078e7fcdabdafa589413819d0013283270ae6d6d
Everton Vieira <vieiraeverton33@hotmail.com>
2022-11-30 14:34:19 -0300

    feat: created Header

---
+++ portifolio/src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import { FiMenu } from "react-icons/fi";
+
+export interface IOpenMenu{
+    isOpen: boolean;
+    setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const Header = ({isOpen, setIsOpen}:IOpenMenu) => {
+
+    return(
+        <header className="flex items-center justify-between mx-8 my-5">
+            <h1 className="text-2xl text-blue-600">Everton Dev</h1>
+            <FiMenu size={25} className="text-slate-100" onClick={() => setIsOpen(!isOpen)}/>
+        </header>
+    )
+};
+
+export default Header;
