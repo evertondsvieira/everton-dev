f988463ddf62faa61f6fbb02e9a06ccc2e1ccd18
Everton Vieira <vieiraeverton33@hotmail.com>
2022-11-30 17:06:19 -0300

    feat: created NavBar

--- portifolio/src/components/NavBar/index.tsx
+++ portifolio/src/components/NavBar/index.tsx
@@ -6,19 +6,19 @@ import { IOpenMenu } from "../Header"
 const NavBar = ({setIsOpen}:IOpenMenu) => {
 
     return(
-        <div className="w-60 h-screen flex items-center justify-center text-xl">
+        <div className="w-[290px] h-screen flex items-center justify-center text-xl">
             <div className="flex-col space-y-6">
-                <div className="w-70 h-full text-slate-100 flex items-center justify-end " onClick={() => setIsOpen(false)}><AiOutlineClose size={25}/></div>
+                <div className="w-70 h-full text-slate-100 flex items-center justify-end cursor-pointer hover:text-blue-600" onClick={() => setIsOpen(false)}><AiOutlineClose size={25}/></div>
                 <ul className="w-54 gap-x-36 flex-col text-slate-100 ">
-                    <li className="hover:text-blue-600">Home</li>
-                    <li className="pt-12 hover:text-blue-600">About</li>
-                    <li className="pt-12 hover:text-blue-600">Education</li>
-                    <li className="pt-12 hover:text-blue-600">Projects</li>
-                    <li className="pt-12 hover:text-blue-600">Contact with me</li>
-                    <li className="flex justify-center gap-12 pt-12 pb-8">
-                        <a href="https://github.com/evertondsvieira" className="hover:text-blue-600"><AiOutlineGithub size={25}/></a>
-                        <a href="https://www.linkedin.com/in/everton-da-silva-vieira/" className="hover:text-blue-600"><AiFillLinkedin size={25}/></a>
-                        <a href="http://instagram.com/evertuuuv" className="hover:text-blue-600"><AiOutlineInstagram size={25}/></a>
+                    <li className="hover:text-blue-600 cursor-pointer">Home</li>
+                    <li className="pt-12 hover:text-blue-600 cursor-pointer">About</li>
+                    <li className="pt-12 hover:text-blue-600 cursor-pointer">Education</li>
+                    <li className="pt-12 hover:text-blue-600 cursor-pointer">Projects</li>
+                    <li className="pt-12 hover:text-blue-600 cursor-pointer">Let's talk</li>
+                    <li className="flex justify-center gap-12 pt-12 pb-8 cursor-pointer">
+                        <a href="https://github.com/evertondsvieira" target="blank" className="hover:text-blue-600 hover:scale-110 hover:delay-150"><AiOutlineGithub size={25}/></a>
+                        <a href="https://www.linkedin.com/in/everton-da-silva-vieira/" target="blank" className="hover:text-blue-600 hover:scale-110 hover:delay-150"><AiFillLinkedin size={25}/></a>
+                        <a href="http://instagram.com/evertuuuv" target="blank" className="hover:text-blue-600 hover:scale-110 hover:delay-150"><AiOutlineInstagram size={25}/></a>
                     </li>
                 </ul>
             </div>
