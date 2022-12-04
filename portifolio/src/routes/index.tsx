b323504dac4f5e2499a34523d0716f2793fe6ea1
Everton Vieira <vieiraeverton33@hotmail.com>
2022-12-04 00:05:42 -0300

    feat: created routes

---
+++ portifolio/src/routes/index.tsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Education from "../pages/Education";
+import Projects from "../pages/Projects";
+import Contact from "../pages/Contact";
+
+const RoutesMain = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/education" element={<Education />} />
+      <Route path="/projects" element={<Projects />} />
+      <Route path="/contact" element={<Contact />} />
+    </Routes>
+  );
+};
+
+export default RoutesMain;
