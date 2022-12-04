dc3e4d6a6c73eef0a8b2542d3372b8787a9cfbc0
Everton Vieira <vieiraeverton33@hotmail.com>
2022-12-04 00:04:44 -0300

    feat: added pages in app.tsx

--- portifolio/src/App.tsx
+++ portifolio/src/App.tsx
@@ -1,15 +1,21 @@
 import "./App.css";
 import "./reset.css";
+
+
 import { useState } from "react";
 import Header from "./components/Header";
-import Dashboard from "./pages/Dashboard";
 import NavBar from "./components/NavBar";
 import Modal from "react-modal";
 Modal.setAppElement("#root");
 
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Education from "./pages/Education";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+
 function App() {
   const [isOpen, setIsOpen] = useState(false);
-
   const customStyles = {
     content: {
       top: "-10px",
@@ -26,10 +32,14 @@ function App() {
   return (
     <div>
       <Header isOpen={isOpen} setIsOpen={setIsOpen} />
-      <Modal isOpen={isOpen} style={customStyles}>
+      <Modal isOpen={isOpen} style={customStyles} closeTimeoutMS={500}>
         <NavBar isOpen={isOpen} setIsOpen={setIsOpen} />
       </Modal>
-      <Dashboard />
+      <Home />
+      <About />
+      <Education />
+      <Projects />
+      <Contact />
     </div>
   );
 }
