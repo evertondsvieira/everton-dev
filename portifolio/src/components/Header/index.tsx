bab3c80ccc29c4258b8d1c6216178a79ea20b07c
Everton Vieira <vieiraeverton33@hotmail.com>
2022-12-04 00:05:58 -0300

    fix: fixed header

--- portifolio/src/components/Header/index.tsx
+++ portifolio/src/components/Header/index.tsx
@@ -6,10 +6,9 @@ export interface IOpenMenu{
 }
 
 const Header = ({isOpen, setIsOpen}:IOpenMenu) => {
-
     return(
         <header className="flex items-center justify-between mx-8 my-5">
-            <h1 className="text-2xl text-blue-600">Everton Dev</h1>
+            <h1 className="text-2xl text-slate-100">Everton Dev</h1>
             <FiMenu size={25} className="text-slate-100 cursor-pointer" onClick={() => setIsOpen(!isOpen)}/>
         </header>
     )
