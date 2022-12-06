1d698b200f22cfe72cea9f5adf5c55d3ad6acd06
Everton Vieira <vieiraeverton33@hotmail.com>
2022-12-06 11:35:28 -0300

    fix: fixed contact

--- portifolio/src/pages/Contact/index.tsx
+++ portifolio/src/pages/Contact/index.tsx
@@ -1,7 +1,23 @@
 const Contact = () => {
     return(
-        <div>
-            <h1 className="text-center text-2xl font-extrabold uppercase text-slate-100">Contact</h1>
+        <div className="text-slate-100">
+            <h1 className="text-left text-2xl mx-4 my-4 underline">Contact me</h1>
+            <h2 className="text-center mx-4 my-4">Have you any question? Let's talk</h2>
+            <div className="flex gap-2 items-center justify-center flex-col">
+                <div>
+                    <p>Your name:</p>
+                    <input type="text"  className="outline-none text-black" autoComplete="off"/>
+                </div>
+                <div>
+                    <p>Your email:</p>
+                    <input type="text" className="outline-none text-black" autoComplete="off"/>
+                </div>
+                <div>
+                    <p>Your message:</p>
+                    <input type="text" className="flex items-start h-40 outline-none text-black" autoComplete="off"/>
+                </div>
+                <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-1 px-4 rounded inline-flex items-center">Send</button>
+            </div>           
         </div>    
     )
 }
