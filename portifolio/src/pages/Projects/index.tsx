d09693bd35100f88ef2e60d6d599c37144b5b91e
Everton Vieira <vieiraeverton33@hotmail.com>
2022-12-24 03:08:12 -0300

    fix: fixed text centralization

--- portifolio/src/pages/Projects/index.tsx
+++ portifolio/src/pages/Projects/index.tsx
@@ -2,7 +2,7 @@ const Projects = () => {
     return(
         <div id="projects" className="text-slate-100">
             <h1 className="text-left text-2xl mx-4 my-4 underline">Portfolio</h1>
-            <h2 className="text-center text-lg mx-4 my-4">My last projects</h2>
+            <h2 className="text-left text-lg mx-4 my-4">My last projects:</h2>
             <div className="flex items-center justify-center flex-row flex-wrap gap-3 max-w-[1200px] mb-8">
                 <div>
                     <p>Comix Shop</p>
