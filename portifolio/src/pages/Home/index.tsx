7b0ad275692bea54ac2bfc40a8663a0a1ff0311d
Everton Vieira <vieiraeverton33@hotmail.com>
2023-01-01 18:25:27 -0300

    Fix: fixed phrase in home

--- portifolio/src/pages/Home/index.tsx
+++ portifolio/src/pages/Home/index.tsx
@@ -1,14 +1,19 @@
 import Typewriter from "typewriter-effect";
+import { AiOutlineHeart } from "react-icons/ai";
 
 const Home = () => {
   return (
     <div id="/" className="text-center pt-16 text-slate-100 max-w-[1200px]">
-      <p>Hi, welcome to my page</p>
+      <div className="flex items-center justify-center gap-1">
+        <p>Hi</p>
+        <AiOutlineHeart />
+      </div>
+      <p>Welcome to my page</p>
       <div className="flex items-center justify-center gap-1">
         <p>I am a</p>
         <Typewriter
           options={{
-            strings: ["Front-end Developer", "Back-end Developer", "Full-stack Developer"],
+            strings: ["Front End Developer", "Back End Developer", "Full Stack Developer"],
             autoStart: true,
             loop: true,
           }}
