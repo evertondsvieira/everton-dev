506461c5dbf8d77307fb02b9273f0f763d9979f4
Everton Vieira <vieiraeverton33@hotmail.com>
2023-01-09 04:41:04 -0300

    Fix: fixed margin

--- portifolio/src/pages/Projects/index.tsx
+++ portifolio/src/pages/Projects/index.tsx
@@ -3,8 +3,8 @@ import { SiExpress, SiCss3, SiJavascript, SiHtml5, SiNodedotjs, SiReact, SiStyle
 const Projects = () => {
     return(
         <div id="projects" className="text-slate-100">
-            <h1 className="text-left text-2xl mx-4 my-4 underline">Portfolio</h1>
-            <h2 className="text-left text-lg mx-4 my-4">My last projects:</h2>
+            <h1 className="text-left text-2xl mx-4 laptop:mx-6 my-4 underline">Portfolio</h1>
+            <h2 className="text-left text-lg mx-4 laptop:mx-6 my-4">My last projects:</h2>
             <div className="w-screen h-auto flex flex-row overflow-x-scroll items-center px-4 pb-4 gap-3 laptop:overflow-x-hidden laptop:flex-wrap laptop:justify-center max-w-[1440px]">
                 <div>
                     <a href="https://evertondsvieira.github.io/Comix-Shop/" target="blank"><img className="max-w-[300px]" src="/comixshop.png" alt=""/></a>
