95f89c8da4f0c3e4ef94a52dad3e68dffbdb5155
Everton Vieira <vieiraeverton33@hotmail.com>
2023-01-09 04:11:45 -0300

    Feat: added scroll

--- portifolio/src/pages/Projects/index.tsx
+++ portifolio/src/pages/Projects/index.tsx
@@ -5,10 +5,9 @@ const Projects = () => {
         <div id="projects" className="text-slate-100">
             <h1 className="text-left text-2xl mx-4 my-4 underline">Portfolio</h1>
             <h2 className="text-left text-lg mx-4 my-4">My last projects:</h2>
-            <div className="flex items-center justify-center flex-row flex-wrap gap-3 max-w-[1440px] mb-8">
+            <div className="w-screen h-auto flex flex-row overflow-x-scroll items-center px-4 pb-4 gap-3 laptop:overflow-x-hidden laptop:flex-wrap laptop:justify-center laptop:max-w-[1440px]">
                 <div>
-                    <p>Comix Shop</p>
-                    <a href="https://evertondsvieira.github.io/Comix-Shop/" target="blank"><img src="/comixshop.png" alt="" /></a>
+                    <a href="https://evertondsvieira.github.io/Comix-Shop/" target="blank"><img className="max-w-[300px]" src="/comixshop.png" alt=""/></a>
                     <div className="flex items-center justify-center m-2 gap-2">
                         <SiHtml5 />
                         <SiCss3 />
@@ -16,8 +15,7 @@ const Projects = () => {
                     </div>
                 </div>
                 <div>
-                    <p>Nu Kenzie</p>
-                    <a href="https://react-entrega-s1-nu-kenzie-evertondsvieira.vercel.app/" target="blank"><img src="/nukenzie.png" alt="" /></a>
+                    <a href="https://react-entrega-s1-nu-kenzie-evertondsvieira.vercel.app/" target="blank"><img className="max-w-[300px]" src="/nukenzie.png" alt="" /></a>
                     <div className="flex items-center justify-center m-2 gap-2">
                         <SiReact />
                         <SiCss3 />
@@ -25,8 +23,7 @@ const Projects = () => {
                     </div>
                 </div>
                 <div>
-                    <p>Kenzie Burger</p>
-                    <a href="https://react-entrega-s1-hamburgueria-da-kenzie-evertondsvieira.vercel.app/" target="blank"><img src="/kenzieburger.png" alt="" /></a>
+                    <a href="https://react-entrega-s1-hamburgueria-da-kenzie-evertondsvieira.vercel.app/" target="blank"><img className="max-w-[300px]" src="/kenzieburger.png" alt="" /></a>
                     <div className="flex items-center justify-center m-2 gap-2">
                         <SiReact />
                         <SiCss3 />
@@ -34,8 +31,7 @@ const Projects = () => {
                     </div>
                 </div>
                 <div>
-                    <p>Kenzie Hub</p>
-                    <a href="https://kenzie-hub-eight-gamma.vercel.app/" target="blank"><img src="/kenziehub.png" alt="" /></a>
+                    <a href="https://kenzie-hub-eight-gamma.vercel.app/" target="blank"><img className="max-w-[300px]" src="/kenziehub.png" alt="" /></a>
                     <div className="flex items-center justify-center m-2 gap-2">
                         <SiReact />
                         <SiStyledcomponents />
@@ -43,8 +39,7 @@ const Projects = () => {
                     </div>
                 </div>
                 <div>
-                    <p>The Cost</p>
-                    <a href="https://thecost.vercel.app/" target="blank"><img src="/thecost.png" alt="" /></a>
+                    <a href="https://thecost.vercel.app/" target="blank"><img className="max-w-[300px]" src="/thecost.png" alt="" /></a>
                     <div className="flex items-center justify-center m-2 gap-2">
                         <SiReact />
                         <SiStyledcomponents />
@@ -52,8 +47,7 @@ const Projects = () => {
                     </div>
                 </div>
                 <div>
-                    <p>Work Collection</p>
-                    <a href="https://github.com/evertondsvieira/WorkCollection" target="blank"><img src="/workcollection.png" alt="" /></a>
+                    <a href="https://github.com/evertondsvieira/WorkCollection" target="blank"><img className="max-w-[300px]" src="/workcollection.png" alt="" /></a>
                     <div className="flex items-center justify-center m-2 gap-2">
                         <SiNodedotjs />
                         <SiExpress />
@@ -63,8 +57,7 @@ const Projects = () => {
                     </div>
                 </div>
                 <div>
-                    <p>Utopic Bank</p>
-                    <a href="" target="blank"><img src="/blank.png" alt="" /></a>
+                    <a href="" target="blank"><img className="max-w-[300px]" src="/blank.png" alt="" /></a>
                     <div className="flex items-center justify-center m-2 gap-2">
                     <SiPostgresql />
                     <SiDocker />
